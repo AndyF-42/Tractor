9dd8ebb24bd7b8665072591960556eeaab01a9ca
Andrew Fleischer <apfleisc@uci.edu>
2024-03-25 15:46:03 -0700

    finished non-UI server

--- server_noui.py
+++ server_noui.py
@@ -7,186 +7,151 @@ import json
 
 
 
-# Start server function
-def start_server():
-    global server
-
-    server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    server.bind((HOST_ADDR, HOST_PORT))
-    server.listen()  # server is listening for client connection
-
-    thread = threading.Thread(target=accept_clients, args=(server,))
-    thread.start()
-
-    print(f"Server listening on address {HOST_ADDR} and port {HOST_PORT}")
-
-
-# Stop server function
-def stop_server():
-    global server, clients, clients_names, player_data
-    
-    for connection in clients:
-        connection.close()
-    clients = []
-    clients_names = []
-
-    server.close()
-    update_clients(clients_names)
-
+class TractorServer():
+    caller = None
     player_data = []
+    first_player = None
+    current_winner = None
+    starting_cards = []
+    winning_cards = []
+    potential_points = 0
+    points_scored = 0
 
-
-# Accept clients to server
-def accept_clients(the_server):
-    while True:
-        if len(clients) < num_players:
-            client, addr = the_server.accept()
-            clients.append(client)
-
-            # start message thread to not clog the gui thread
-            thread = threading.Thread(target=communicate, args=(client,))
-            thread.start()
-
-
-# Receive and send messages with current client
-def communicate(client):
-    global server, clients, pot, caller, current_winner, winning_cards, first_player, starting_cards, potential_points, points_scored
-
-    # send welcome message to client
-    name = client.recv(4096).decode('utf-8')
-    
-    client.send(("welcome-" + str(len(clients)-1) + "-" + "-".join(clients_names)).encode('utf-8')) # e.g., welcome-2-Andy-Michael
-
-    for i, c in enumerate(clients):
-        if c != client:
-            c.send(("joined-" + str(i) + "-" + str(len(clients)-1) + "-" + name).encode('utf-8')) # e.g., joined-[my_index]-[newplayer_index]-[name] 
-    
-    clients_names.append(name)
-    update_clients(clients_names)  # update client names display
-
-    if len(clients) == num_players:
-        sleep(1)
-
-        for c in clients[:-1]:
-            c.send("start-wait".encode('utf-8'))
-        client.send("start-play".encode('utf-8'))
-
-    while True:
-        message = client.recv(4096).decode('utf-8')
-        if not message:
-            break
-
-        if message == "draw":
-            card = deck.draw()
-            client.send(("draw-" + card.get_rank() + "-" + card.get_suit()).encode('utf-8')) # e.g., "draw-THREE-CLUBS"
-            #client.send(pickle.dumps(card))
-            
-            if deck.isempty():
-                for c in clients:
-                    if c == caller:
-                        c.send(("pot-" + str(len(pot)) + "-" + "-".join([card.get_rank() + "-" + card.get_suit() for card in pot])).encode('utf-8'))
-                    else:
-                        c.send("dealover".encode('utf-8'))
-            else:
-                clients[(clients.index(client) + 1) % len(clients)].send("yourturn".encode('utf-8'))
+    def __init__(self, num_players):
+        self.num_players = num_players
+        self.deck = Deck(num_players // 2)
+        self.deck.shuffle()
+        self.pot = [self.deck.draw() for _ in range(2 * num_players)]
         
-        elif message.startswith("call"): # e.g., call-JOKER-RED-TEN-HEARTS
-            caller = client
-            for c in clients:
-                if c != client: # send non-callers the info
-                    c.send(("call-" + name + "-" + "-".join(message.split("-")[1:])).encode('utf-8'))
-                elif deck.isempty(): # if deck empty, send the pot to the caller
-                    c.send(("pot-" + str(len(pot)) + "-" + "-".join([card.get_rank() + "-" + card.get_suit() for card in pot])).encode('utf-8'))
+        with open("secrets.json", "r") as secrets:
+            self.address, self.port = json.load(secrets).values()
+    
+    # run the server
+    def run(self):
+        self.server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.server.bind((self.address, self.port))
+        self.server.listen()
+
+        print(f"Server listening on address {self.address} and port {self.port}")
         
-        elif message.startswith("bury"):
-            message = message.split("-")
-            pot = [Card(message[i], message[i+1]) for i in range(2, len(message), 2)]
-            for c in clients:
-                if c == client:
-                    c.send("play-yourturn".encode('utf-8'))
+        self.clients = []
+        self.clients_names = []
+        while True:
+            if len(self.clients) < self.num_players:
+                client, addr = self.server.accept()
+                self.clients.append(client)
+
+                thread = threading.Thread(target=self.communicate, args=(client,))
+                thread.start() 
+
+    # receive and send messages with a single client
+    def communicate(self, client):
+        name = client.recv(4096).decode('utf-8') # start with client's name
+        
+        client.send(("welcome-" + str(len(self.clients)-1) + "-" + "-".join(self.clients_names)).encode('utf-8')) # e.g., welcome-2-Andy-Michael
+
+        for i, c in enumerate(self.clients):
+            if c != client:
+                c.send(("joined-" + str(i) + "-" + str(len(self.clients)-1) + "-" + name).encode('utf-8')) # e.g., joined-[my_index]-[newplayer_index]-[name] 
+        
+        self.clients_names.append(name)
+        print(f"{name} joined!")
+
+        if len(self.clients) == self.num_players:
+            sleep(1)
+
+            for c in self.clients[:-1]:
+                c.send("start-wait".encode('utf-8'))
+            client.send("start-play".encode('utf-8'))
+
+        while True:
+            message = client.recv(4096).decode('utf-8')
+            if not message:
+                break
+
+            if message == "draw":
+                card = self.deck.draw()
+                client.send(("draw-" + card.get_rank() + "-" + card.get_suit()).encode('utf-8')) # e.g., "draw-THREE-CLUBS"
+                
+                if self.deck.isempty():
+                    for c in self.clients:
+                        if c == self.caller:
+                            c.send(("pot-" + str(len(self.pot)) + "-" + "-".join([card.get_rank() + "-" + card.get_suit() for card in self.pot])).encode('utf-8'))
+                        else:
+                            c.send("dealover".encode('utf-8'))
                 else:
-                    c.send("play".encode('utf-8'))
-        
-        elif message.startswith("play"): # e.g., play-TEN-HEARTS-TEN-HEARTS
-            message = message.split("-")
-            cards = [Card(message[i], message[i+1]) for i in range(1, len(message), 2)]
-
-            for card in cards:
-                potential_points += card.get_points()
-
-            if not starting_cards:
-                first_player = client
-                starting_cards = [card for card in cards]
-
-            if is_better(winning_cards, cards):
-                current_winner = client
-                winning_cards = [card for card in cards]
-
-            for c in clients:
-                if c != client:
-                    c.send(("put-" + name + "-" + "-".join(message[1:]) +
-                           ("-STARTING-" + "-".join([card.get_rank() + "-" + card.get_suit() for card in starting_cards])) + 
-                           ("-GO" if c == clients[(clients.index(client) + 1) % len(clients)] else "")).encode('utf-8'))
+                    self.clients[(self.clients.index(client) + 1) % len(self.clients)].send("yourturn".encode('utf-8'))
             
-            if first_player == clients[(clients.index(client) + 1) % len(clients)]: # made it all around
-                for c in clients:
-                    if c == current_winner:
-                        c.send(("winner-You" + ("-POINTS" if potential_points > 0 else "")).encode('utf-8'))
-                    else:
-                        c.send(("winner-" + clients_names[(clients.index(current_winner))] + ("-POINTS" if potential_points > 0 else "")).encode('utf-8'))
-                first_player = None
-                starting_cards = []
-                winning_cards = []
-        
-        elif message.startswith("score"): # e.g, score-True or score-False
-            if message.split("-")[1] == "True":
-                points_scored += potential_points
-
-            if points_scored >= 80:
-                for c in clients:
-                    c.send("gameover-attack".encode('utf-8'))
-            else:
-                for c in clients:
-                    if c == current_winner:
-                        c.send((message + "-" + str(points_scored) + "-GO").encode('utf-8'))
-                    else:
-                        c.send((message + "-" + str(points_scored)).encode('utf-8'))
-
-            current_winner = None
-            potential_points = 0
-
+            elif message.startswith("call"): # e.g., call-JOKER-RED-TEN-HEARTS
+                self.caller = client
+                for c in self.clients:
+                    if c != client: # send non-callers the info
+                        c.send(("call-" + name + "-" + "-".join(message.split("-")[1:])).encode('utf-8'))
+                    elif self.deck.isempty(): # if deck empty, send the pot to the caller
+                        c.send(("pot-" + str(len(self.pot)) + "-" + "-".join([card.get_rank() + "-" + card.get_suit() for card in self.pot])).encode('utf-8'))
             
+            elif message.startswith("bury"):
+                message = message.split("-")
+                self.pot = [Card(message[i], message[i+1]) for i in range(2, len(message), 2)]
+                for c in self.clients:
+                    if c == client:
+                        c.send("play-yourturn".encode('utf-8'))
+                    else:
+                        c.send("play".encode('utf-8'))
+            
+            elif message.startswith("play"): # e.g., play-TEN-HEARTS-TEN-HEARTS
+                message = message.split("-")
+                cards = [Card(message[i], message[i+1]) for i in range(1, len(message), 2)]
 
-    clients_names.remove(client)
-    clients.remove(client)
-    client.close()
-    update_clients(clients_names)
+                for card in cards:
+                    self.potential_points += card.get_points()
+
+                if not self.starting_cards:
+                    self.first_player = client
+                    self.starting_cards = [card for card in cards]
+
+                if is_better(self.winning_cards, cards):
+                    self.current_winner = client
+                    self.winning_cards = [card for card in cards]
+
+                for c in self.clients:
+                    if c != client:
+                        c.send(("put-" + name + "-" + "-".join(message[1:]) +
+                            ("-STARTING-" + "-".join([card.get_rank() + "-" + card.get_suit() for card in self.starting_cards])) + 
+                            ("-GO" if c == self.clients[(self.clients.index(client) + 1) % len(self.clients)] else "")).encode('utf-8'))
+                
+                if self.first_player == self.clients[(self.clients.index(client) + 1) % len(self.clients)]: # made it all around
+                    for c in self.clients:
+                        if c == self.current_winner:
+                            c.send(("winner-You" + ("-POINTS" if self.potential_points > 0 else "")).encode('utf-8'))
+                        else:
+                            c.send(("winner-" + self.clients_names[(self.clients.index(self.current_winner))] + ("-POINTS" if self.potential_points > 0 else "")).encode('utf-8'))
+                    self.first_player = None
+                    self.starting_cards = []
+                    self.winning_cards = []
+            
+            elif message.startswith("score"): # e.g, score-True or score-False
+                if message.split("-")[1] == "True":
+                    self.points_scored += self.potential_points
+
+                if self.points_scored >= 80:
+                    for c in self.clients:
+                        c.send("gameover-attack".encode('utf-8'))
+                else:
+                    for c in self.clients:
+                        if c == self.current_winner:
+                            c.send((message + "-" + str(self.points_scored) + "-GO").encode('utf-8'))
+                        else:
+                            c.send((message + "-" + str(self.points_scored)).encode('utf-8'))
+
+                self.current_winner = None
+                self.potential_points = 0
+
+        self.clients_names.remove(client)
+        self.clients.remove(client)
+        client.close()
 
 
-# Update the client list display
-def update_clients(names):
-    MainWindow.clients_list.setText("\n".join(names))
-
-
-
-server = None
-with open("secrets.json", "r") as secrets:
-    HOST_ADDR, HOST_PORT = json.load(secrets).values()
-clients = []
-caller = None
-clients_names = []
-player_data = []
-num_players = 4
-deck = Deck(num_players // 2)
-deck.shuffle()
-pot = []
-for _ in range(2 * num_players):
-    pot.append(deck.draw())
-first_player = None
-current_winner = None
-starting_cards = []
-winning_cards = []
-potential_points = 0
-points_scored = 0
-
-sys.exit(app.exec())
+if __name__ == "__main__":
+    server = TractorServer(4)
+    server.run()
